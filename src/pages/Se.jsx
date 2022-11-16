e6ead99bb94bbb49b4ec8ad0c5eb6f4f88c9f4dc
Stenstromen <peppoji@gmail.com>
2022-11-16 23:17:02 +0100

    API integration

--- src/pages/Se.jsx
+++ src/pages/Se.jsx
@@ -1,8 +1,29 @@
+import React from "react"
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const URL = process.env.REACT_APP_BACKEND_URL;
+const CONFIG = {
+    headers: {
+        authorization: process.env.REACT_APP_AUTHORIZATION,
+    }
+}
 
 function Se() {
+
+    const [dates, setDates] = useState([]);
+
+    useEffect(() => {
+      axios.get(URL+"/se",CONFIG).then((response) => {
+        setDates(response.data);
+        console.log(response.data)
+      });
+    }, []);
+
     return (
     <div>
       <h1>.SE</h1>
+      <p>{dates}</p>
     </div>
   );
 }
