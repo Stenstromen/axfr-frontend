ed09a8de12f3bc6bed3a22c4675cb80646a5d5a6
Stenstromen <peppoji@gmail.com>
2022-11-18 17:15:15 +0100

    Added more api integration

--- src/pages/Se.jsx
+++ src/pages/Se.jsx
@@ -1,29 +1,39 @@
-import React from "react"
+import React from "react";
 import { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 const URL = process.env.REACT_APP_BACKEND_URL;
 const CONFIG = {
-    headers: {
-        authorization: process.env.REACT_APP_AUTHORIZATION,
-    }
-}
+  headers: {
+    authorization: process.env.REACT_APP_AUTHORIZATION,
+  },
+};
 
 function Se() {
+  const [dates, setDates] = useState([]);
 
-    const [dates, setDates] = useState([]);
+  useEffect(() => {
+    axios.get(URL + "/se", CONFIG).then((response) => {
+      setDates(response.data);
+    });
+  }, []);
 
-    useEffect(() => {
-      axios.get(URL+"/se",CONFIG).then((response) => {
-        setDates(response.data);
-        console.log(response.data)
-      });
-    }, []);
-
-    return (
+  return (
     <div>
       <h1>.SE</h1>
-      <p>{dates}</p>
+      <Container>
+        <ul>
+          {dates.map((item) => {
+            return (
+              <li key={item.date}>
+                <Link to={`/se/${item.date}`}>{item.date} {item.amount}</Link>
+              </li>             
+            );
+          })}
+        </ul>
+      </Container>
     </div>
   );
 }
--- src/pages/SeDomains.jsx
+++ src/pages/SeDomains.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import axios from "axios";
+
+const URL = process.env.REACT_APP_BACKEND_URL;
+const CONFIG = {
+  headers: {
+    authorization: process.env.REACT_APP_AUTHORIZATION,
+  },
+};
 
 function SeDomains() {
   const { param } = useParams();
+  const [page, setPage] = useState(0);
+  const [domains, setDomains] = useState([]);
+
+  useEffect(() => {
+    axios.get(URL + `/se/${param}/${page}`, CONFIG).then((response) => {
+      setDomains(response.data);
+    });
+  }, []);  
 
   return (
     <div>
       <h1>the param is {param}</h1>
+      <h1>.SE</h1>
+      <Container>
+        <ul>
+          {domains.map((item) => {
+            return (
+              <li key={item.domain}>
+                <p>{item.domain}</p>
+              </li>             
+            );
+          })}
+        </ul>
+      </Container>
     </div>
   );
 }
