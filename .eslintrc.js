4f1a3c6de83e90e4a49d1f5ef90c6c0e7abeec7e
Stenstromen <peppoji@gmail.com>
2022-12-12 21:13:54 +0100

    Fixed eslint jest env

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,9 @@ module.exports = {
     browser: true,
     node: true,
     es2021: true,
+    jest: true
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
-  overrides: [
-    {
-      jest: true,
-    },
-  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
