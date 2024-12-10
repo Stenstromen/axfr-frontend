9d972505f92df1338311219173fd0660bef290e2
Stenstromen <kontakt@filipstenstrom.se>
2024-12-11 00:15:53 +0100

    Fixed navbar

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -5,42 +5,36 @@ import { Link } from "react-router-dom";
 
 function MobileNavbar({ darkmode, locations }) {
   return (
-    <>
-      <Nav className="me-auto">
-        <NavDropdown
-          title={
-            <span style={{ color: "white" }}>
-              Menu
-            </span>
-          }
-          id="basic-nav-dropdown"
-          menuVariant={darkmode ? "light" : "dark"}
-          style={{
-            backgroundColor: darkmode ? "#0d6efd" : "#212529",
-          }}
-        >
-          <div style={{
-            backgroundColor: darkmode ? "white" : "#212529",
-          }}>
-            {locations.map((location, index) => (
-              <Nav.Link as={Link} key={index} to={location.path}>
-                <NavDropdown.Item
-                  style={{
-                    backgroundColor: darkmode ? "white" : "#212529",
-                  }}
-                >
-                  <span style={{
-                    color: darkmode ? "black" : "white",
-                  }}>
-                    {location.name}
-                  </span>
-                </NavDropdown.Item>
-              </Nav.Link>
-            ))}
-          </div>
-        </NavDropdown>
-      </Nav>
-    </>
+    <Nav className="me-auto">
+      <NavDropdown
+        title={
+          <span style={{ color: "white" }}>
+            Menu
+          </span>
+        }
+        id="basic-nav-dropdown"
+        menuVariant={darkmode ? "light" : "dark"}
+        style={{
+          backgroundColor: darkmode ? "#0d6efd" : "#212529",
+          padding: "8px 0",
+        }}
+      >
+        {locations.map((location, index) => (
+          <NavDropdown.Item 
+            key={index}
+            as={Link}
+            to={location.path}
+            style={{
+              backgroundColor: darkmode ? "white" : "#212529",
+              color: darkmode ? "black" : "white",
+              padding: "8px 0",
+            }}
+          >
+            {location.name}
+          </NavDropdown.Item>
+        ))}
+      </NavDropdown>
+    </Nav>
   );
 }
 
