55c5cb957900fa31bfa64c3b09d24a90aa8b2ae5
Stenstromen <kontakt@filipstenstrom.se>
2024-12-11 13:11:08 +0100

    CSS sticky nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useDefaultProvider } from "../contexts/default";
 import { Container, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -9,7 +9,6 @@ import MobileNavbar from "./MobileNavbar";
 import DesktopNavbar from "./DesktopNavbar";
 
 function NavBar() {
-  const [sticky, setSticky] = useState(false);
   const { isMobile, darkmode, setDarkmode } = useDefaultProvider();
 
   const locations = [
@@ -20,27 +19,14 @@ function NavBar() {
     { name: "Domain Stats", path: "/stats" },
   ];
 
-  function stickNavbar() {
-    if (window !== undefined) {
-      let windowHeight = window.scrollY;
-      windowHeight ? setSticky(true) : setSticky(false);
-    }
-  }
-
-  useEffect(() => {
-    window.addEventListener("scroll", stickNavbar);
-    return () => window.removeEventListener("scroll", stickNavbar);
-  }, []);
-
   return (
-    <div>
+    <div style={{ position: "sticky", top: 0, zIndex: 1020 }}>
       <Helmet>
         <meta name="theme-color" content={darkmode ? "#0d6efd" : "#212529"} />
       </Helmet>
       <Navbar
         bg={darkmode ? "primary" : "dark"}
         variant={darkmode ? "light" : "dark"}
-        fixed={sticky ? "top" : ""}
       >
         <Container>
           <img
